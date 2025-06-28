af3ae863d4daa4574e472a090e061bb6a6f38b67
ErickMadrid <127989412+ErickMadrid@users.noreply.github.com>
2025-06-27 23:51:17 -0300

    Update TokenA.sol

--- TokenA.sol
+++ TokenA.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenA is ERC20 {
-    constructor() ERC20("Token A", "TKA") {
-        _mint(msg.sender, 1_000_000 ether); // 1 millón de tokens con 18 decimales
+contract TakoA is ERC20, Ownable, ERC20Permit {
+    constructor(address recipient, address initialOwner)
+        ERC20("TakoA", "MTK")
+        Ownable(initialOwner)
+        ERC20Permit("TakoA")
+    {
+        _mint(recipient, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
